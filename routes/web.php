ec0dc328c33ba2521e17591dc7b9bddb855890b5
Gavin Kimpson <gkimpson@gmail.com>
2025-09-17 11:41:09 +0100

    feat: added throttling to the requests for the /lookup route in web.php

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,5 @@
 use App\Http\Controllers\LookupController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/lookup', [LookupController::class, '__invoke']);
+Route::get('/lookup', [LookupController::class, '__invoke'])
+    ->middleware('throttle:lookup-throttle');
